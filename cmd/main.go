d8801cbf91058d9a50ff5e47763d34606172a4e8
anatolethien <anatole.thien@ynov.com>
2021-08-16 04:37:37 +0200

    Frontend ok

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/anatolethien/ascii-art"
 	"github.com/anatolethien/go-reloaded"
 	"html/template"
 	"net/http"
 )
 
+type display struct {
+	Output string
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var ascii = display{Output: ascii.Ascii(reloaded.Split(" ", "\\n"), "standard")}
 		var t, _ = template.ParseFiles("./templates/index.html")
-		t.Execute(w, nil)
+		t.Execute(w, ascii)
 	})
 	http.HandleFunc("/ascii-art", func(w http.ResponseWriter, r *http.Request) {
-		var input = reloaded.Split(r.FormValue("input"), "\\n")
-		var banner = r.FormValue("banner")
-		type display struct{ Output string }
+		var input, banner = reloaded.Split(r.FormValue("input"), "\\n"), r.FormValue("banner")
 		var ascii = display{Output: ascii.Ascii(input, banner)}
 		var t, _ = template.ParseFiles("./templates/index.html")
 		t.Execute(w, ascii)
 	})
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	fmt.Printf("Launching web app at http://127.0.0.1:3000...\n")
 	http.ListenAndServe(":3000", nil)
 }
